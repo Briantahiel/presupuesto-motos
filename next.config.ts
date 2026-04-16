325ca4c22b7fe130271f77d79355477a4bc22ba8
Brian Gomez <brianmgz2012@gmail.com>
2026-04-16 15:34:46 -0300

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,45 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "offlineCache",
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 días
-        },
+runtimeCaching: [
+  {
+    urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "google-fonts",
+      expiration: {
+        maxEntries: 10,
+        maxAgeSeconds: 60 * 60 * 24 * 365,
       },
     },
-  ],
+  },
+  {
+    urlPattern: /\/_next\/static\//,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "next-static",
+    },
+  },
+  {
+    urlPattern: /\/_next\/image\//,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "next-images",
+    },
+  },
+  {
+    urlPattern: /^https?.*/,
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "http-cache",
+      networkTimeoutSeconds: 5,
+      expiration: {
+        maxEntries: 200,
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+      },
+    },
+  },
+]
 });
 
 module.exports = withPWA({
